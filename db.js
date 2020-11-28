a3c023f59eadb15e9a34a612f30f55ec6073194e
Hetav Desai <hetav.desai20@gmail.com>
2020-11-28 20:12:09 +0000

    Initial commit

---
+++ db.js
@@ -0,0 +1,84 @@
+db = {
+    "suits": [{
+    question: `Harvey Specter is considered one of New York's most brilliant corporate litigation lawyers, but what's his unusual middle name?
+    (a) Michael
+    (b) Reginald
+    (c) Brandon
+    `,
+    answer: "b",
+  },
+  {
+    question: `Which partner loves mudding and had a cat named Bruno?
+    (a) Harvey Specter
+    (b) Louis Litt
+    (c) Jessica Pearson
+    `,
+    answer: "b",
+  },
+  {
+    question: `Which English lawyer described Donna Paulsen as having \"a body like Elizabeth Hurley and the sass of a Maggie Thatcher\" in season 3?
+    (a) Michael Ross
+    (b) Harvey Specter
+    (c) Stephen Huntley
+    `,
+    answer: "c",
+  },
+  {
+    question: `Where did Harvey Specter work before he became an associate at Pearson Hardman?
+    (a) New York District Attorney's Office
+    (b) Bratton Gould
+    (c) Darby International
+    `,
+    answer: "a",
+  },
+  {
+    question: `Jessica Pearson and Daniel Hardman ousted three named partners in order to create Pearson Hardman. What was the firm called before the ousting?
+    (a) Specter Zane Litt
+    (b) Gordon Schmidt Van Dyke
+    (c) Pearson Hardman
+    `,
+    answer: "b",
+  },
+  ],
+  "siliconvalley": [{
+    question: `Silicon Valley Q1
+    (a) Michael
+    (b) Reginald
+    (c) Brandon
+    `,
+    answer: "b",
+  },
+  {
+    question: `Which partner loves mudding and had a cat named Bruno?
+    (a) Harvey Specter
+    (b) Louis Litt
+    (c) Jessica Pearson
+    `,
+    answer: "b",
+  },
+  {
+    question: `Which English lawyer described Donna Paulsen as having \"a body like Elizabeth Hurley and the sass of a Maggie Thatcher\" in season 3?
+    (a) Michael Ross
+    (b) Harvey Specter
+    (c) Stephen Huntley
+    `,
+    answer: "c",
+  },
+  {
+    question: `Where did Harvey Specter work before he became an associate at Pearson Hardman?
+    (a) New York District Attorney's Office
+    (b) Bratton Gould
+    (c) Darby International
+    `,
+    answer: "a",
+  },
+  {
+    question: `Jessica Pearson and Daniel Hardman ousted three named partners in order to create Pearson Hardman. What was the firm called before the ousting?
+    (a) Specter Zane Litt
+    (b) Gordon Schmidt Van Dyke
+    (c) Pearson Hardman
+    `,
+    answer: "b",
+  },
+  ]
+}
